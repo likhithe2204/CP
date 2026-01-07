4f169c0f92d2a1751109beaf3b36fe2c9267ec63
Likhith Edupuganti <159748439+likhithe2204@users.noreply.github.com>
2026-01-07 11:47:50 +0530

    5^n mod 100 = 25 for all n ≥ 2.

---
+++ 800_Rated/AgainTwentyFive.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class AgainTwentyFive {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        System.out.println("25");
+    }
+}
