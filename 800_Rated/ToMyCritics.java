1c17bf30f5e0c41be3662244e0dc8f7f37baafc9
Likhith Edupuganti <159748439+likhithe2204@users.noreply.github.com>
2026-01-09 12:30:59 +0530

    If any two of a, b, c sum to 10 or more, print YES

---
+++ 800_Rated/ToMyCritics.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class ToMyCritics {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        while (t-- > 0) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int c = scanner.nextInt();
+
+            if (a + b >= 10 || b + c >= 10 || c + a >= 10) System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+}
